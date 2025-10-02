dd8199609b9338b435296e405447d49757fce66d
TheBestPessimist <cristian@tbp.land>
2025-10-02 16:34:34 +0300

    idk what i'm doing. i'm trying to make node run this crap.

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -7,12 +7,14 @@ kotlin {
     js {
         outputModuleName = "shared"
         browser()
-        binaries.library()
+        this.nodejs()
+        // binaries.library()
         generateTypeScriptDefinitions()
         compilerOptions {
             target = "es2015"
         }
     }
+
 
     sourceSets {
         all {
@@ -26,6 +28,12 @@ kotlin {
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test")) // Brings all the platform dependencies automatically
+        }
+        jsMain.dependencies {
+            implementation(npm("react", "> 14.0.0 <=16.9.0"))
         }
     }
 }
