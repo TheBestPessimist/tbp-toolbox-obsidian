9429e0ed96c0b1d42c6dc38fee6e271ee733ea6d
TheBestPessimist <cristian@tbp.land>
2025-10-23 19:49:01 +0300

    warnings

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -53,7 +53,10 @@ kotlin {
 			implementation(devNpm("@types/node", "^16.11.6"))
 			implementation(devNpm("@typescript-eslint/eslint-plugin", "5.29.0"))
 			implementation(devNpm("@typescript-eslint/parser", "5.29.0"))
+			implementation(devNpm("eslint", "^8.0.0"))
 			implementation(devNpm("obsidian", "latest"))
+			implementation(devNpm("@codemirror/state", "6.5.0"))
+			implementation(devNpm("@codemirror/view", "6.38.1"))
 			implementation(devNpm("tslib", "2.4.0"))
 			implementation(devNpm("typescript", "4.7.4"))
 		}
