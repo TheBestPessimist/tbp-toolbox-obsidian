765c07fd9e0a544e9a9c913a98ecc8d9408b5c04
TheBestPessimist <cristian@tbp.land>
2025-11-02 22:45:50 +0200

    Add `PinFileOnTabChange`

--- src/jsMain/kotlin/land/tbp/toolbox/PinFileOnTabChange.kt
+++ src/jsMain/kotlin/land/tbp/toolbox/PinFileOnTabChange.kt
@@ -3,6 +3,9 @@ package land.tbp.toolbox
 import obsidian.App
 import obsidian.EventRef
 
+/**
+ * This can be used instead of the plugin Mononote ([https://github.com/czottmann/obsidian-mononote](https://github.com/czottmann/obsidian-mononote)).
+ */
 fun pinFileOnTabChange(app: App): EventRef =
     app.workspace.on("active-leaf-change", { leaf ->
         leaf ?: return@on
