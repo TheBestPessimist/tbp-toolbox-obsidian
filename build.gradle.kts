106c2a128638fbe6a52ba92aea2767401d9d07b9
TheBestPessimist <cristian@tbp.land>
2025-11-02 13:54:37 +0200

    Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -63,8 +63,6 @@ kotlin {
 			// implementation(devNpm("@typescript-eslint/eslint-plugin", "5.29.0"))
 			// implementation(devNpm("@typescript-eslint/parser", "5.29.0"))
 			// implementation(devNpm("eslint", "^8.0.0"))
-			// implementation(devNpm("@codemirror/state", "6.5.0"))
-			// implementation(devNpm("@codemirror/view", "6.38.1"))
 			// implementation(devNpm("tslib", "2.4.0"))
 			// implementation(devNpm("typescript", "4.7.4"))
 		}
--- obsidian-api/build.gradle.kts
+++ obsidian-api/build.gradle.kts
@@ -1,42 +1,45 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 plugins {
-	alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.kotlinMultiplatform)
 }
 
 kotlin {
-	js {
-		// This is just an API definition library, not a runnable module
-		// We don't set outputModuleName because this won't be compiled to a standalone module
-		
-		// Use nodejs target for compatibility
-		nodejs {
-		}
+    js {
+        // This is just an API definition library, not a runnable module
+        // We don't set outputModuleName because this won't be compiled to a standalone module
 
-		// This is a library of external declarations only
-		binaries.library()
+        // Use nodejs target for compatibility
+        nodejs {
+        }
 
-		compilerOptions {
-			// Target ES2015 for modern JavaScript features
-			target = "es2015"
+        // This is a library of external declarations only
+        binaries.library()
 
-			// Use CommonJS module format to match the main project
-			moduleKind = org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_COMMONJS
-		}
-	}
+        compilerOptions {
+            // Target ES2015 for modern JavaScript features
+            target = "es2015"
 
-	// Apply compiler options to all source sets
-	compilerOptions {
-		apiVersion = KotlinVersion.KOTLIN_2_2
-		languageVersion = KotlinVersion.KOTLIN_2_2
-	}
+            // Use CommonJS module format to match the main project
+            moduleKind = org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_COMMONJS
+        }
+    }
 
-	sourceSets {
-		jsMain {
-			dependencies {
-				api(kotlin("stdlib-js"))
+    // Apply compiler options to all source sets
+    compilerOptions {
+        apiVersion = KotlinVersion.KOTLIN_2_2
+        languageVersion = KotlinVersion.KOTLIN_2_2
+    }
+
+    sourceSets {
+        jsMain {
+            dependencies {
+                api(kotlin("stdlib-js"))
                 implementation(devNpm("obsidian", "latest"))
-			}
-		}
-	}
+                implementation(devNpm("@codemirror/state", "6.5.0"))
+                implementation(devNpm("@codemirror/view", "6.38.1"))
+                implementation(devNpm("moment", "2.30.1"))
+            }
+        }
+    }
 }
